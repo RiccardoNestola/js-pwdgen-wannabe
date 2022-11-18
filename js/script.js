7639c1dcd9e57e685c13bb42f8ed38464b02604a
RiccardoNestola <riccardinux@gmail.com>
2022-11-18 17:14:02 +0100

    add css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-
+document.body.addEventListener("pointermove", (e)=>{
+    const { currentTarget: el, clientX: x, clientY: y } = e;
+    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+    el.style.setProperty('--posX',  x-l-w/2);
+    el.style.setProperty('--posY',  y-t-h/2);
+  })
 
 const nome = prompt ("Dimmi io tuo nome!");
 const cognome = prompt ("Dimmi il tuo cognome!");
@@ -6,4 +11,4 @@ const colore = prompt ("Qual'è il tuo colore preferito?");
 let password = nome + cognome + colore + 22;
 
 
-document.getElementById(`psw`).innerHTML = `Ciao la tua password è ` + password;
+document.getElementById(`psw`).innerHTML = `Ciao, la tua password è ` + password;
