52b04bde747210a56e0904168ee4fc0f59184725
RiccardoNestola <riccardinux@gmail.com>
2022-11-18 15:37:14 +0100

    var to let

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const nome = prompt ("Dimmi io tuo nome!");
 const cognome = prompt ("Dimmi il tuo cognome!");
 const colore = prompt ("Qual'è il tuo colore preferito?");
-var password = nome + cognome + colore + 22;
+let password = nome + cognome + colore + 22;
 
 
 document.getElementById(`psw`).innerHTML = `Ciao la tua password è ` + password;
