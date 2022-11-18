114c1a3bd83c03f32ebca98115ed6f75357d78c0
RiccardoNestola <riccardinux@gmail.com>
2022-11-18 15:34:14 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+const nome = prompt ("Dimmi io tuo nome!");
+const cognome = prompt ("Dimmi il tuo cognome!");
+const colore = prompt ("Qual'è il tuo colore preferito?");
+var password = nome + cognome + colore + 22;
+
+
+document.getElementById(`psw`).innerHTML = `Ciao la tua password è ` + password;
